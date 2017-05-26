e0cd33e24e0c694fb3480bf1100117ae2b45a31c
Bhargav Mangipudi <bhargav.mangipudi@gmail.com>
2017-05-26 04:11:21 -0500

    Dependencies with JniHelpers.

--- jni_wrapper/src/main/cpp/HelloJNI.cpp
+++ jni_wrapper/src/main/cpp/HelloJNI.cpp
@@ -1,9 +1,13 @@
 #include "HelloJNI.h"
 #include <iostream>
+#include <srmp/FactorTypes/PottsType.h>
+#include "JniHelpers.h"
 
 using namespace std;
+using namespace srmpLib;
 
 JNIEXPORT void JNICALL Java_HelloJNI_sayHello(JNIEnv *, jobject) {
+    PottsFactorType* potts = NULL;
     cout << "Hello World from C++!" << endl;
     return;
 }
